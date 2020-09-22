85d71fed23da9bdab12016c497c40702e8242e86
Barsik-cmd <71725965+Barsik-cmd@users.noreply.github.com>
2020-09-22 21:19:09 +0300

    Add files via upload

---
+++ Lab5Kuzmin/src/Utility/Console.java
@@ -0,0 +1,12 @@
+package Utility;
+
+import java.util.Scanner;
+
+public class Console {
+    static Scanner in = new Scanner(System.in);
+
+    public static String read(){
+        System.out.print("~ ");
+        return in.nextLine();
+    }
+}
